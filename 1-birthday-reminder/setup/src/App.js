316b5280734f3cf8b23bfe9cfb126325d83c8f51
mattyocode <matthewoliver@live.co.uk>
2021-04-04 19:14:46 +0100

    first commit

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+        <List />
+        <button onClick={() => console.log("clicked")}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
